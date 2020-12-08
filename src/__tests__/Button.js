a85f6fff00804a7b4b2d6bd33ebe60a4f6ed7769
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2020-12-08 05:05:17 +0100

    Add button component

---
+++ src/__tests__/Button.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Button from "../components/Button";
+
+describe("Button Component", () => {
+  const props = {
+    className: "",
+    disabled: false,
+    handleClick: jest.fn(),
+    label: "Find Repositories",
+    testId: "find_repositories",
+    type: "button"
+  };
+
+  test("render the correct label", () => {
+    const { getByRole } = render(<Button {...props} />);
+    expect(getByRole("button")).toHaveTextContent(props.label);
+  });
+
+  test("invoke an API request when clicked", () => {
+    const { getByTestId } = render(<Button {...props} />);
+    fireEvent.click(getByTestId("find_repositories"));
+    expect(props.handleClick).toBeCalled();
+  });
+});
